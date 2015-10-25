ae41c094ab5abfaf1f8f46aec2a6b214acd21d14
sina sima <simacna@gmail.com>
2015-10-25 12:14:56 -0400

    first commit

---
+++ scraper.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+def fetch_search_results(query=None, minAsk=None, maxAsk=None, bedrooms=None):
+    search_params = {
+        key: val for key, val in locals().items() if val is not None #locals() returns dictionary of current namespace
+    }
+    if not search_params:
+        raise ValueError("No valid keywords")
+
+    base = 'https://newyork.craigslist.org/search/aap'
+    resp = requests.get(base, params=search_params, timeout=3)
+    resp.raise_for_status()  # <- no-op if status==200
+    return resp.content, resp.encoding
+
+def parse_source(html, encoding='utf-8'):
+    parsed = BeautifulSoup(html, from_encoding=encoding)
+    return parsed
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        html, encoding = read_search_results()
+    else:
+        html, encoding = fetch_search_results(
+            minAsk=500, maxAsk=1000, bedrooms=2
+        )
+    doc = parse_source(html, encoding)
+    print doc.prettify(encoding=encoding)
